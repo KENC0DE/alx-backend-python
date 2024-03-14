d072a916c27f3190df318f752890ced6db2a4a56
KENC0DE <kenansameseret@gmail.com>
2024-03-14 18:14:50 +0530

    sum float list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Static Lists
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    input_list: given list of floats
+    return: the sum of the list
+    """
+    return sum(input_list)
